fd1a71fce3524759f2ee7beab06e9c68f57905e7
Daniel Oliveira <daniel.fao@gmail.com>
2017-06-13 15:10:36 -0300

    Login mock screen

---
+++ WeVolunteer/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  WeVolunteer
+//
+//  Created by Daniel Oliveira on 6/13/17.
+//  Copyright © 2017 Daniel Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
